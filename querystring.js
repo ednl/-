ac5ec57f3b2506600516b2423a5b43e638a79a90
Ewoud Dronkert <ewoud@dronkert.net>
2020-08-03 17:09:26 +0200

    Documenting

--- querystring.js
+++ querystring.js
@@ -1,44 +1,106 @@
+/**
+ * Javascript class for managing URL query strings.
+ * You can:
+ *   read the current query string from the URL;
+ *   read, add, delete or edit individual parameters;
+ *   make a whole new query string;
+ *   output the complete query string as plain text;
+ *   output the complete query string encoded for use in an HTML link;
+ *   navigate to the current page with a specific query string.
+ * @class
+ */
 class QueryString {
+
+	/**
+	 * Reads or sets a new query string. If used as an empty constructor,
+	 * will load the query string from the current document URL. If an object
+	 * is passed, a new query string will be initialised from it.
+	 * @constructs
+	 * @param {object} obj - Optional initialiser for new query string.
+	 */
 	constructor(obj) {
+
 		if (arguments.length) {
+
+			// If an object was passed, use it to initialise the .values member.
 			this.values = typeof obj === 'object' ? obj : {};
+
 		} else {
+
+			// No parameters to the constructor => parse the query string
+			// from the current document URL.
 			this.values = {};
-			const q = window.location.search.substr(1).split('&');
-			for (const p of q) {
+			const pars = window.location.search.substr(1).split('&');
+			for (const p of pars) {
+
 				// Can't use split() because the limit is two parts, but
 				// the second part may include the separator (and more),
 				// and split() would cut that off.
 				const m = p.match(/^([^=]*)=?(.*)$/m);
+
 				// Index 1 is the key, index 2 is the value. Both can be
 				// empty, but an empty key makes no sense.
-				if (Array.isArray(m) && m.length == 3 && m[1].length) {
-					let v = decodeURIComponent(m[2]);
-					const n = v.match(/^-?(0|[1-9]\d*)/);
-					if (n) {
-						if (n[0].length == v.length) {
-							// Complete match for valid integer
-							v = parseInt(v);
-						} else if (v.substr(n[0].length).match(/^(\.\d+)?(e[+-]?\d+)?$/i)) {
-							// The rest completely matches a valid fractional part and/or exponent
-							v = parseFloat(v);
+				if (m && m.length == 3 && m[1].length) {
+
+					const key = decodeURIComponent(m[1]);
+					let val = decodeURIComponent(m[2]);
+
+					// Try to convert string values to integer, float, boolean
+					// or null. No conversion if not a complete & valid match.
+					const im = val.match(/^-?(0|[1-9]\d*)/);
+					if (im) {
+
+						if (im[0].length == val.length) {
+
+							// The whole thing completely matches
+							// a valid integer.
+							val = parseInt(val);
+
+						} else {
+
+							const sub = val.substr(im[0].length);
+							const fm = sub.match(/^(\.\d+)?(e[+-]?\d+)?$/i);
+							if (fm) {
+
+								// The rest completely matches a valid
+								// fractional part and/or exponent.
+								val = parseFloat(val);
+
+							}
 						}
+
 					} else {
-						const s = v.toLowerCase();
-						if (s === 'true') {
-							v = true;
-						} else if (s === 'false') {
-							v = false;
-						} else if (s === 'null') {
-							v = null;
+
+						// Check for boolean or null values as strings.
+						if (val === 'true') {
+
+							val = true;
+
+						} else if (val === 'false') {
+
+							val = false;
+
+						} else if (val === 'null') {
+
+							val = null;
+
 						}
 					}
-					this.values[decodeURIComponent(m[1])] = v;
+
+					// Store in .values member object.
+					this.values[key] = val;
 				}
 			}
 		}
 	}
 
+	/**
+	 * TODO Description.
+	 * @method
+	 * @param {string} [arg0='&'] - Separator between query parameters.
+	 * @param {string} [arg1='='] - Separator between keys and values.
+	 * @param {string} [arg2='?'] - Prefix of the query string.
+	 */
 	serialize(...args) {
 		const sep = args.length ? args.shift() : '&';
 		const eq = args.length ? args.shift() : '=';
@@ -59,46 +121,106 @@ class QueryString {
 		return '';
 	}
 
+	/**
+	 * TODO Description.
+	 * @method
+	 * @returns {string} Complete query string as plain text.
+	 */
 	toText() {
 		return this.serialize('&');
 	}
 
+	/**
+	 * TODO Description.
+	 * @method
+	 * @returns {string} Complete query string as HTML encoded text.
+	 */
 	toHtml() {
 		return this.serialize('&amp;');
 	}
 
+	/**
+	 * TODO Description.
+	 * @method
+	 * @returns {string} Complete query string as HTML encoded text.
+	 */
 	toString() {
 		return this.toHtml();
 	}
 
+	/**
+	 * TODO Description.
+	 * @method
+	 */
 	goto() {
 		location = this.toText();
 	}
 
+	/**
+	 * TODO Description.
+	 * @method
+	 * @param {string} key - Name of the query parameter.
+	 * @returns {number} Integer value of the query parameter, or undefined.
+	 */
 	getInt(key) {
 		return key in this.values ? parseInt(this.values[key]) : undefined;
 	}
 
+	/**
+	 * TODO Description.
+	 * @method
+	 * @param {string} key - Name of the query parameter.
+	 * @returns {number} Float value of the query parameter, or undefined.
+	 */
 	getFloat(key) {
 		return key in this.values ? parseFloat(this.values[key]) : undefined;
 	}
 
+	/**
+	 * TODO Description.
+	 * @method
+	 * @param {string} key - Name of the query parameter.
+	 * @returns {boolean} Boolean value of the query parameter, or undefined.
+	 */
 	getBool(key) {
 		return key in this.values ? !!(this.values[key]) : undefined;
 	}
 
+	/**
+	 * TODO Description.
+	 * @method
+	 * @param {string} key - Name of the query parameter.
+	 * @returns {string} String value of the query parameter, or undefined.
+	 */
 	getStr(key) {
 		return key in this.values ? '' + this.values[key] : undefined;
 	}
 
+	/**
+	 * TODO Description.
+	 * @method
+	 * @param {string} key - Name of the query parameter.
+	 * @returns Value of the query parameter, any type, could be undefined.
+	 */
 	get(key) {
 		return this.values[key];
 	}
 
+	/**
+	 * TODO Description.
+	 * @method
+	 * @param {string} key - Name of the query parameter.
+	 * @param val - Value of the query parameter, any assignable type.
+	 */
 	set(key, val) {
 		this.values[key] = val;
 	}
 
+	/**
+	 * TODO Description.
+	 * @method
+	 * @param {string} key - Name of the query parameter.
+	 */
 	unset(key) {
 		delete this.values[key];
 	}
